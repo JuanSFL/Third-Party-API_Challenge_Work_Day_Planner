6c4c1413defffc7cb539e114263cdf3df1b82b05
JuanSFL <molina98j@yahoo.com>
2022-10-28 15:52:22 -0400

    added current date and time with ticking clock and local storage functionality. Also changed some styling properties and added comments

---
+++ Assets/script.js
@@ -0,0 +1,33 @@
+//displays current date and time
+var todayDate = moment().format('dddd, MMM Do YYYY LTS ');
+$("#currentDay").html(todayDate);
+// variable to make the clock tick every second
+var clockTick = setInterval( () => {
+    currentDay.innerHTML = moment().format(" dddd, MMM Do YYYY LTS ")} )
+
+
+$(document).ready(function () {
+    // saveBtn click function 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+   
+    // Get item from local storage
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
+    timeTracker();
+})
